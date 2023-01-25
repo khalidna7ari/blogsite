9d28857515fb5e57cf0fb26ba466db94f15b2db4
khalidna7ari <kmnahari89@gmail.com  git config --global user.name khalidna7ari>
2023-01-25 19:18:12 +0000

    ✨ - add hero component and ammend header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   const navItems: string[] = ['about me', 'blog', 'projects', 'contact']
 
   return (
-    <header className="px-6 py-4 flex justify-between items-center">
+    <header className="py-4 flex justify-between items-center">
       <h2 className="bg-slate-500 h-10 w-10 rounded-full font-black flex items-center justify-center text-white bg-gradient-to-tr from-fuchsia-500 to-rose-500">K</h2>
       <nav>
         <ul className="flex gap-6 capitalize text-sm underline underline-offset-8 decoration-2 font-medium">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
 export default function Home() {
 
@@ -12,8 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className='max-w-7xl mx-auto px-6'>
+        <Header />
+        <div className='max-w-4xl mx-auto'>
+          <Hero />
 
-      <Header />
+        </div>
+      </div>
     </>
   )
 }
