3707752caac8298de9c17733f482f6a9bf74a66e
khalidna7ari <kmnahari89@gmail.com  git config --global user.name khalidna7ari>
2023-01-25 18:15:55 +0000

    ✨ - Add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
-const Header = () => (
-  <header>
-    <p>This is the header sections</p>
-  </header>
-);
+const Header = () => {
+  const navItems: string[] = ['about me', 'blog', 'projects', 'contact']
+
+  return (
+    <header className="px-6 py-4 flex justify-between items-center">
+      <h2 className="bg-slate-500 h-10 w-10 rounded-full font-black flex items-center justify-center text-white bg-gradient-to-tr from-fuchsia-500 to-rose-500">K</h2>
+      <nav>
+        <ul className="flex gap-6 capitalize text-sm underline underline-offset-8 decoration-2 font-medium">
+          {navItems.map((navItem, id) => (
+            <li key={id} className=" hover:text-rose-500 active:text-rose-500"><a href="#">{navItem}</a></li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+};
 
 export default Header;
